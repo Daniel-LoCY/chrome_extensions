21bed22b78daa242de1b74075f4d1d0ebcbb5fc3
Game Game <game48875@gmail.com>
2022-06-05 22:44:25 +0800

    連接API成功

--- first_extensions/test.js
+++ first_extensions/test.js
@@ -1,9 +1,21 @@
-$('#btn').click(function(){
-    $.ajax({
-        url: "http://20.80.46.248:5000/watch?v=hC5nETdPFEw&ab_channel=郭董懂不懂",
-        type: "GET",
-        success: function(data){
-            alert(data)
-        }
-    })
+let url = ""
+let download_url = "http://daniel0422.ddns.net/";
+let original_url = "https://www.youtube.com/"
+window.onload = function(){
+  chrome.tabs.query({active:true, lastFocusedWindow:true}, tabs => {
+    url = tabs[0].url
+    if(url.includes(original_url)){
+      let s = url.split("&")
+      url = s[0]
+      url = url.replace(original_url, download_url)
+    }
+    else{
+      $("#download").hide()
+      $("div").append(`<p>此網站不支援<p>`)
+    }
+  })
+}
+  
+$("#download").click(function(){
+  location.href = url
 })

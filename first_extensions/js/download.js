a7407130a60f4cc94ef7673bed4be276293ba98a
Game Game <game48875@gmail.com>
2022-08-01 19:56:12 +0800

    complete

--- first_extensions/js/download.js
+++ first_extensions/js/download.js
@@ -1,5 +1,5 @@
 // let url = 'http://127.0.0.1:5000/'
-let url = 'https://heroku--html.herokuapp.com/'
+let url = 'http://20.80.46.248:5000/'
 const a = location.search.substring(3, )
 url = url + a
 location.href = url
--- first_extensions/js/popup.js
+++ first_extensions/js/popup.js
@@ -2,7 +2,7 @@ let url = ""
 let original_url_video = "https://www.youtube.com/watch"
 let original_url_playlist = "https://www.youtube.com/playlist"
 
-let api_host = 'https://heroku--html.herokuapp.com'
+let api_host = 'http://20.80.46.248:5000'
 // let api_host = 'http://127.0.0.1:5000'
 
 const download = () => {
@@ -12,10 +12,12 @@ const download = () => {
       if(url.includes(original_url_playlist)){
         document.querySelector('#res').classList.add('d-none')
         document.querySelector("#download").value = `開始下載播放清單`
-        get_size(url)
+        // get_size(url)
+        get_res('', '1')
       }
       else{
-        get_size(url)
+        // get_size(url)
+        get_res(url)
       }
       let s = url.split("m/")[1]
       url = s.split('&')[0]
@@ -42,31 +44,37 @@ function newWindow(res=0){
   if(res != ''){
     open_url += `&res=${res}`
   }
-  if(localStorage.getItem('need_email') == 'true'){
-    open_url += `&email=${email}`
-  }
   window.open(open_url, '', strWindowFeatures)
 }
 
-
-
-function get_res(url){
-  $.ajax({
-    url : `${api_host}/res?url=${url}`,
-    type: 'GET',
-    async: false,
-    success: function(data){
-      $.each(data.res, function(i, item){
-        let select = document.querySelector("#select")
-        let option = document.createElement('option')
-        option.innerHTML = item
-        option.value = item
-        select.append(option)
-        if(i == data.res.length-1)
-          document.querySelector("#download").value = `直接下載最高畫質(${item})`
-      })
-    }
+const enable_button =  () => {
+  document.querySelector("#download").addEventListener('click', function(){newWindow('')})
+  document.querySelector("#download_res").addEventListener('click', function(){download_res()})
+  let button = document.querySelectorAll('input[type=button]')
+  button.forEach(e => {
+    e.disabled = false
   })
+}
+
+function get_res(url, type=''){
+  if(type == ''){
+    $.ajax({
+      url : `${api_host}/res?url=${url}`,
+      type: 'GET',
+      success: function(data){
+        $.each(data.res, function(i, item){
+          let select = document.querySelector("#select")
+          let option = document.createElement('option')
+          option.innerHTML = item
+          option.value = item
+          select.append(option)
+          if(i == data.res.length-1)
+            document.querySelector("#download").value = `直接下載最高畫質(${item})`
+        })
+      }
+    }).then(enable_button())
+  }
+  else{enable_button()}
 }
 
 function download_res(){
@@ -101,39 +109,35 @@ function load(){
 }
 
 window.onload = () => {
-  load()
+  // load()
+  document.querySelector('#main').classList.remove('d-none')
   let button = document.querySelectorAll('input[type=button]')
   button.forEach(e => {
     e.disabled = true
   })
+  download()
 }
 
-function get_size(url, _async=true){
-    $.ajax({
-      url: `${api_host}/get_size?url=${url}`,
-      type: 'GET',
-      // async: _async,
-      success: function(data){
-        if(data.size > 360){
-          localStorage.setItem('need_email', true)
-        }
-        else{
-          localStorage.setItem('need_email', false)
-        }
-      }
-    }).then(() => {
-      if(_async==true)get_res(url)
-      document.querySelector("#download").addEventListener('click', function(){newWindow('')})
-      document.querySelector("#download_res").addEventListener('click', function(){download_res()})
-      let button = document.querySelectorAll('input[type=button]')
-      button.forEach(e => {
-        e.disabled = false
-      })
-    })
-}
-
-document.querySelector('a').addEventListener('click', () => {
-    // document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; //清除cookue
-    localStorage.removeItem('email')
-    load()
-})
+// function get_size(url, _async=true){
+//     $.ajax({
+//       url: `${api_host}/get_size?url=${url}`,
+//       type: 'GET',
+//       // async: _async,
+//       success: function(data){
+//         if(data.size > 360){
+//           localStorage.setItem('need_email', true)
+//         }
+//         else{
+//           localStorage.setItem('need_email', false)
+//         }
+//       }
+//     }).then(() => {
+//       if(_async==true)get_res(url)
+//       document.querySelector("#download").addEventListener('click', function(){newWindow('')})
+//       document.querySelector("#download_res").addEventListener('click', function(){download_res()})
+//       let button = document.querySelectorAll('input[type=button]')
+//       button.forEach(e => {
+//         e.disabled = false
+//       })
+//     })
+// }
